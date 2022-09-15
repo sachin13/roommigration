5dfa7329292db02496572c54ee9a02b9f9e200be
sachinkhairnar143 <sachinkhairnar4321@gmail.com>
2022-09-15 13:13:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/roommigration/User.kt
@@ -0,0 +1,16 @@
+package com.example.roommigration
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = false)
+    val email: String,
+    val username: String,
+    @ColumnInfo(name = "created", defaultValue = "0")
+    val createdAt: Long = System.currentTimeMillis()
+)
+
